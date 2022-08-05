6faedda6790e36d49d4b62b0dde656f2124fb1a0
markSAO-F <mark@sao.network>
2022-08-05 16:10:13 +0800

    [bug] fileinfo api nil panic fix && fail to store file when filename contains special characters && monitor change - increase the interval of listening download event

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -64,7 +64,7 @@ func (m *Monitor) Run() {
 
 	fmt.Println("listen download status")
 	s := gocron.NewScheduler(time.UTC)
-	s.Every(1).Seconds().Do(func() {
+	s.Every(5).Seconds().Do(func() {
 		finishPurchase, got := m.Model.GetNextPurchaseOrderToFinish()
 		if got {
 			err := m.Finish(int64(finishPurchase.Id))
